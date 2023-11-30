cff0cc3944d0aea509e16299ab7b0c3006ba41fe
Abner Silva <abner.barbosa@sou.inteli.edu.br>
2023-11-29 22:23:36 -0300

    Update Lista circular

--- src/com/projetoListaCircular/ListaCircular.java
+++ src/com/projetoListaCircular/ListaCircular.java
@@ -1,5 +1,61 @@
 package com.projetoListaCircular;
 
 public class ListaCircular<T> {
-    private No
+    private No<T> cabeca;
+    private No<T> cauda;
+
+    private int tamanhoLista;
+
+    public int size(){
+        return this.tamanhoLista;
+    }
+
+    public boolean isEmpty(){
+        return this.tamanhoLista == 0 ? true:false;
+    }
+
+    private No<T> getNo(int index){
+        if(this.isEmpty())
+            throw new IndexOutOfBoundsException("A lista está vazia !");
+
+        if(index == 0){
+            return this.cauda;
+        }
+
+        No<T> noAuxiliar = this.cauda;
+        for(int i = 0;(i < index) && (noAuxiliar != null); i++){
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+
+        return noAuxiliar;
+    }
+
+    public T get(int index){
+        return this.getNo(index).getConteudo();
+    }
+
+    public void remove(int index){
+        if(index >= this.tamanhoLista)
+            throw new IndexOutOfBoundsException("O indice é maior que o tamanho da lista");
+
+        No<T> noAuxiliar = this.cauda;
+        if(index == 0){
+            this.cauda = this.cauda.getProximoNo();
+            this.cabeca.setProximoNo(this.cauda);
+        }else if(index == 1){
+            this.cauda.setProximoNo(this.cauda.getProximoNo().getProximoNo());
+        }else{
+            for (int i = 0; i < index - 1; i++){
+                noAuxiliar = noAuxiliar.getProximoNo();
+            }
+            noAuxiliar.setProximoNo(noAuxiliar.getProximoNo().getProximoNo());
+        }
+
+        this.tamanhoLista--;
+
+
+
+    }
+
+
 }
